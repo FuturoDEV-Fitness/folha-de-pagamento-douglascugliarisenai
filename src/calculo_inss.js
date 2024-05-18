69e8f84e4f8f91276cf7a65596c4bcf10b73cd75
douglascugliaridev <douglas.cugliari@gmail.com>
2024-05-17 21:57:28 -0300

    fix: 🐛 M2S01-ajuste-calculo-inss
    M2S01 Ajuste no calculo do INSS
    
    ✅ Closes: M2S01-ex2

--- src/calculo_inss.js
+++ src/calculo_inss.js
@@ -1,13 +1,19 @@
 module.exports = function calculoInss(salario) {
- if (salario <= 1412.0) {
-  return salario * 0.08;
- } else if (salario >= 1412.0 && salario <= 2666.68) {
-  return salario * 0.09;
- } else if (salario >= 2666.68 && salario <= 4000.03) {
-  return salario * 0.11;
- } else {
-  return (
-   1412.81 * 0.08 + (salario - 1412.81) * 0.09 + (salario - 2666.68) * 0.11
-  );
- }
+    let inss = 0;
+    const TETO_INSS = 908.85;
+
+    if (salario <= 1412.0) {
+        inss = salario * 0.075;
+    } else if (salario >= 1412.01 && salario <= 2666.68) {
+        inss = salario * 0.09;
+    } else if (salario >= 2666.69 && salario <= 4000.03) {
+        inss = salario * 0.12;
+    } else {
+        inss = salario * 0.14;
+    }
+
+    if (inss >= TETO_INSS) inss = TETO_INSS;
+
+    return inss
+
 };
